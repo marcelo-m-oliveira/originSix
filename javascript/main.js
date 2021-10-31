dbf878705c7110516d80a4c7dca06a6f1a35b855
marcelo-m-oliveira <marcelomra.oliveira@gmail.com>
2021-10-31 11:22:00 -0300

    Criando a section serviços

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 const links = document.querySelectorAll('nav ul li a')
+const header = document.querySelector('#header')
+const navHeight = header.scrollHeight
+
 
 for(const element of toggle) {
   element.addEventListener('click', function () {
@@ -13,3 +16,11 @@ for(const link of links){
     nav.classList.remove('show')
   })
 }
+
+window.addEventListener('scroll', function() {
+  if(window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
