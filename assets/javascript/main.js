b3f8f4abe93127ac9a11bfee4e1bb03d9e54cf72
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2021-12-09 21:47:16 -0300

    Criando o footer e adicionando animaçoes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,7 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 const links = document.querySelectorAll('nav ul li a')
-const header = document.querySelector('#header')
-const navHeight = header.scrollHeight
+
 
 
 for(const element of toggle) {
@@ -17,13 +16,17 @@ for(const link of links){
   })
 }
 
-window.addEventListener('scroll', function() {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.scrollHeight
+
   if(window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+
+}
 
 /** Swiper */
 
@@ -50,5 +53,23 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services .header, #services .card,
   #testimonials .header, #testimonials .testimonials,
-  #contact .text, #contact .links`
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `
   , {interval:100})
+
+  /** Back to top */
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top');
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function() {
+  changeHeaderWhenScroll()
+  backToTop()
+})
